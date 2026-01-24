7f5ac63ae7e17dead6712a17476476783c6b333d
slink7 <slink794@gmail.com>
2026-01-24 18:55:03 +0100

    Added wordpress service

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 set -e
 
-# Créer le dossier du socket MySQL
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
-# Initialisation si la base n'existe pas
 if [ ! -d "/var/lib/mysql/mysql" ]; then
   echo "🟡 Initialisation de MariaDB..."
 
   mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-  mariadbd --user=mysql --datadir=/var/lib/mysql --skip-networking &
+  mysqld --user=mysql --skip-networking &
   pid="$!"
 
   sleep 5
 
   mysql -u root << EOF
-CREATE DATABASE IF NOT EXISTS inception_db;
-CREATE USER IF NOT EXISTS 'inception_user'@'%' IDENTIFIED BY 'inception_pwd';
-GRANT ALL PRIVILEGES ON inception_db.* TO 'inception_user'@'%';
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
@@ -29,6 +28,5 @@ EOF
 fi
 
 echo "🚀 Lancement MariaDB"
-#exec mariadbd --user=mysql
 exec mysqld --user=mysql
 
---
+++ srcs/requirements/wordpress/tools/init.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -e
+
+echo "🚀 Lancement WordPress (PHP-FPM)"
+
+# Vérifier les variables
+if [ -z "$DB_HOST" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ] || [ -z "$DB_NAME" ]; then
+  echo "❌ Variables DB manquantes — vérifie ton fichier .env"
+  exit 1
+fi
+
+# Initialiser le volume WordPress s'il est vide
+if [ ! -f /var/www/html/wp-config.php ]; then
+  echo "📂 Initialisation du volume WordPress"
+  cp -r /usr/src/wordpress/* /var/www/html
+fi
+
+# Attendre MariaDB
+until mariadb -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" --ssl=OFF -e "SELECT 1;" >/dev/null 2>&1; do
+  echo "⏳ En attente de MariaDB..."
+  sleep 2
+done
+
+# Créer wp-config.php si absent
+if [ ! -f /var/www/html/wp-config.php ]; then
+  echo "🛠 Création du wp-config.php"
+  cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+  sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wp-config.php
+  sed -i "s/username_here/$DB_USER/" /var/www/html/wp-config.php
+  sed -i "s/password_here/$DB_PASSWORD/" /var/www/html/wp-config.php
+  sed -i "s/localhost/$DB_HOST/" /var/www/html/wp-config.php
+fi
+
+exec php-fpm82 -F
